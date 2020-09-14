946adbfa1d0363b707cd86cc5fd0b045a4db2ccd
Javier Zabala <javier_bislip@hotmail.com>
2020-09-14 09:58:54 -0400

    Changed DB config variables

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 443),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '102b7b75d0bf7811ca1dbf00c46c51c4'),
+      secret: env("ADMIN_JWT_SECRET", "102b7b75d0bf7811ca1dbf00c46c51c4"),
     },
   },
 });
