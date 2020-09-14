cacc6cdea3a0999c0096ef4ee1ba56563f26c05c
Javier Zabala <javier_bislip@hotmail.com>
2020-09-14 09:48:26 -0400

    DB connection

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         uri: env("DATABASE_URI", ""),
-        /* host: env("DATABASE_HOST", "127.0.0.1"),
+        host: env("DATABASE_HOST", "127.0.0.1"),
         srv: env.bool("DATABASE_SRV", false),
         port: env.int("DATABASE_PORT", 27017),
         database: env("DATABASE_NAME", ""),
         username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""), */
+        password: env("DATABASE_PASSWORD", ""),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
