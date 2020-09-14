deed1edc4c86c3340f3742daaf9384164ef36b12
Javier Zabala <javier_bislip@hotmail.com>
2020-09-14 08:29:28 -0400

    DB connection

--- config/database.js
+++ config/database.js
@@ -1,22 +1,26 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "mongoose",
-      settings: {
-        uri: env("DATABASE_URI", ""),
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        srv: env.bool("DATABASE_SRV", false),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", ""),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
-      },
-      options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        ssl: env.bool("DATABASE_SSL", true),
-        useUnifiedTopology: env("USE_UNIFIED_TOPOLOGY", true),
+module.exports = ({ env }) => {
+  console.log(env);
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "mongoose",
+        settings: {
+          uri: env("DATABASE_URI", ""),
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          srv: env.bool("DATABASE_SRV", false),
+          port: env.int("DATABASE_PORT", 27017),
+          database: env("DATABASE_NAME", ""),
+          username: env("DATABASE_USERNAME", ""),
+          password: env("DATABASE_PASSWORD", ""),
+        },
+        options: {
+          authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+          ssl: env.bool("DATABASE_SSL", true),
+          useUnifiedTopology: env("USE_UNIFIED_TOPOLOGY", true),
+        },
       },
     },
-  },
-});
+  };
+};
